84bae2d5030de039ba5b4b24dc89260941df4404
Lancent Yuan <v-layuan@microsoft.com>
2019-06-26 15:24:22 +0800

    profile the waitress

--- run_waitress_server.py
+++ run_waitress_server.py
@@ -1,6 +1,10 @@
 #run_waitress_server.py
-import os  
-from waitress import serve  
+import os
+from waitress import serve
 from App import app
+from werkzeug.contrib.profiler import ProfilerMiddleware
 
-serve(app,host="0.0.0.0",port=os.environ["PORT"]) 
+app.config['PROFILE'] = True
+app.wsgi_app = ProfilerMiddleware(app.wsgi_app, restrictions=[30])
+serve(app,host="0.0.0.0",port=os.environ["PORT"])
+#serve(app,host="0.0.0.0")
